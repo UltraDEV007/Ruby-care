c8d4247d192e3d31dd4d3bd79b1c71fb0eae47c1
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-02 12:29:52 -0500

    give box shadow to footer for both dark and light mode

--- client/src/layouts/Layout/Footer.jsx
+++ client/src/layouts/Layout/Footer.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
@@ -6,28 +6,35 @@ import HomeIcon from "@material-ui/icons/Home";
 import ForumIcon from "@material-ui/icons/Forum";
 import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
-
-const useStyles = makeStyles({
-  root: {
-    width: "100vw",
-    position: "fixed",
-    bottom: 0,
-    marginTop: "20px",
-  },
-  footer: {
-    height: "50px",
-    marginTop: "20px",
-  },
-});
+import { indigo, blue } from "@material-ui/core/colors";
+import { DarkModeContext } from "../../Context/DarkMode/DarkModeContext";
 
 function Footer() {
+  const [darkMode] = useContext(DarkModeContext);
+  const useStyles = makeStyles({
+    root: {
+      width: "100vw",
+      position: "fixed",
+      bottom: 0,
+      marginTop: "20px",
+      boxShadow:
+        darkMode === "dark"
+          ? `2px 2px 3px 2px ${indigo[50]}`
+          : `2px 2px 3px 2px ${blue[600]}`,
+    },
+    footer: {
+      height: "50px",
+      marginTop: "20px",
+    },
+  });
   const classes = useStyles();
+  const history = useHistory();
+
   const routerMap = {
     0: "/",
     1: "/insights",
     2: "/settings",
   };
-  const history = useHistory();
   const [value, setValue] = useState(() => {
     if (history.location.pathname === "/insights") {
       return 1;
