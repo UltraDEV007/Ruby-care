bc3ffedc881e223fc9793764335e39ca5a09e8e0
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-03 14:39:14 -0500

    minimal styling to not found page

--- client/src/screens/Error/NotFound.jsx
+++ client/src/screens/Error/NotFound.jsx
@@ -3,14 +3,47 @@ import { NavLink, useLocation } from "react-router-dom";
 import { checkValidity } from "../../utils/checkValidity";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import { DarkModeContext } from "../../components/Context/DarkModeContext";
 
 const ErrorWrapper = styled.div`
+  min-height: 100vh;
+
+  a {
+    text-decoration: none;
+  }
+  .text-container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+  }
+
   .title {
+    margin-top: 20%;
     font-size: 1.2rem;
+    font-family: "montserrat", sans-serif;
+  }
+
+  .paragraph {
+    font-size: 0.5rem;
   }
   @media screen and (min-width: 1000px) {
     .title {
-      font-size: 2rem;
+      margin-top: 5%;
+      font-size: 3rem;
+    }
+    .paragraph {
+      font-size: 1.5rem;
+    }
+    @media screen and (min-width: 1600px) {
+      .title {
+        margin-top: 5%;
+        font-size: 5rem;
+      }
+      .paragraph {
+        font-size: 3.5rem;
+      }
     }
   }
 `;
@@ -23,9 +56,20 @@ function NotFound() {
   ) : (
     <>
       <ErrorWrapper>
-        <Typography className="title">ERROR 404,</Typography>
-        <p>Sorry, {location.pathname.replace("/", "")} doesn't exist!</p>
-        <NavLink to="/"> Go Back </NavLink>
+        <div className="text-container">
+          <Typography className="title">ERROR 404,</Typography>
+          <Typography className="paragraph">
+            Sorry, {location.pathname.replace("/", "")} doesn't exist!
+          </Typography>
+          <Button
+            variant="outlined"
+            color="secondary"
+            component={NavLink}
+            to="/"
+          >
+            Go Back
+          </Button>
+        </div>
       </ErrorWrapper>
     </>
   );
