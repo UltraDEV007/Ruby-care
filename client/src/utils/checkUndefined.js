684a8a8c9a891b7c660828fa67e421784babf410
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-04 14:07:33 -0500

    deleted comments

--- client/src/utils/checkUndefined.js
+++ client/src/utils/checkUndefined.js
@@ -1,11 +1,3 @@
-// this gets rid of undefined error when searching a edit path by id of a food that is deleted/doesn't exist
-// if (oneFood?.name === undefined) {
-//   window.history.back();
-// } else {
-//   const { name, time, rating } = oneFood;
-//   setFormData({ name, time, rating });
-// }
-
 const goOn = (oneParam, contentParam, setParam) => {
   contentParam = oneParam;
   setParam(contentParam);
