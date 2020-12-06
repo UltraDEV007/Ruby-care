21aac3e5acc3881e805bb4fffb849ef8ca9769ee
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-06 09:40:59 -0500

    added animation for rx-guide logo from p2

--- client/src/components/MedComponents/RXGuideLogo.jsx
+++ client/src/components/MedComponents/RXGuideLogo.jsx
@@ -4,10 +4,33 @@ import styled from "styled-components";
 const Div = styled.div`
   margin-top: -20px;
   margin-bottom: -50px;
+  @keyframes bounce {
+    0%,
+    20%,
+    60%,
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+    }
+
+    40% {
+      -webkit-transform: translateY(-20px);
+      transform: translateY(-20px);
+    }
+
+    80% {
+      -webkit-transform: translateY(-10px);
+      transform: translateY(-10px);
+    }
+  }
 `;
 const Logo = styled.img`
   width: 40px;
   height: 40px;
+  &:hover {
+    animation: bounce 1s;
+    cursor: pointer;
+  }
 `;
 
 const Text = styled.img`
