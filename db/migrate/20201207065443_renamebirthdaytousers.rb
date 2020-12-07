1ef9f350de20cc3e8408adb525211d0ba480d379
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-07 06:27:49 -0500

    heroku

--- db/migrate/20201207065443_renamebirthdaytousers.rb
+++ db/migrate/20201207065443_renamebirthdaytousers.rb
@@ -1,5 +1,5 @@
 class Renamebirthdaytousers < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :birthday, :datetime, :limit => nil 
+    change_column :users, :birthday, :date, :limit => nil 
   end
 end
