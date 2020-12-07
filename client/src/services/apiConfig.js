8423f3c7d9b7a36748e9d0e50240124cb542cc5a
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-07 06:36:34 -0500

    changed api config

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://care-app-api.herokuapp.com/"
+    ? "https://heroku-care.herokuapp.com/"
     : "http://localhost:3000";
 
 const api = axios.create({
