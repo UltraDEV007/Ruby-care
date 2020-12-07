e0d1e5330aeb67f6f2fb98fca0ccaafc5c256a33
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-07 01:00:22 -0500

    feature complete, delete insight modal is now on insightDetail.jsx

--- client/src/screens/InsightScreens/InsightDetail.jsx
+++ client/src/screens/InsightScreens/InsightDetail.jsx
@@ -10,6 +10,7 @@ import { grey, yellow } from "@material-ui/core/colors";
 import { goBack } from "../../utils/goBack";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import DeleteInsightFromDetail from "../../components/Modals/DeleteInsightFromDetail";
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -103,12 +104,28 @@ const Wrapper = styled.div`
     }
   }
 `;
+
 export default function InsightDetail({ getOneInsight, handleDelete }) {
   const [insight, setInsight] = useState(null);
   const [currentUser] = useContext(CurrentUserContext);
   const [darkMode] = useContext(DarkModeContext);
   const [loaded, setLoaded] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+
   const { id } = useParams();
+
+  const onDelete = (id) => {
+    handleDelete(id);
+    setOpenDelete(false);
+  };
+
+  const handleDeleteOpen = (id) => {
+    setOpenDelete(id);
+  };
+
+  const handleDeleteClose = () => {
+    setOpenDelete(false);
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -130,58 +147,68 @@ export default function InsightDetail({ getOneInsight, handleDelete }) {
   }
 
   return (
-    <Wrapper darkMode={darkMode}>
-      <div className="content-container">
-        <div className="title-container">
-          <Typography className="title">&nbsp;{insight?.title}</Typography>
-          <Link className="link" to={`/users/${insight?.user?.id}`}>
-            <Typography className="user-name">
-              <AccountCircleIcon className="user-icon" />
-              &nbsp;{insight.user?.name}
+    <>
+      <Wrapper darkMode={darkMode}>
+        <div className="content-container">
+          <div className="title-container">
+            <Typography className="title">&nbsp;{insight?.title}</Typography>
+            <Link className="link" to={`/users/${insight?.user?.id}`}>
+              <Typography className="user-name">
+                <AccountCircleIcon className="user-icon" />
+                &nbsp;{insight.user?.name}
+              </Typography>
+            </Link>
+            <Typography>
+              Created At:&nbsp;
+              <Moment format="dddd, MMMM Do yyyy">
+                <small>{insight?.created_at}</small>
+              </Moment>
             </Typography>
-          </Link>
-          <Typography>
-            Created At:&nbsp;
-            <Moment format="dddd, MMMM Do yyyy">
-              <small>{insight?.created_at}</small>
-            </Moment>
-          </Typography>
+          </div>
+          {insight?.user_id === currentUser?.id && (
+            <>
+              <div className="buttons2">
+                <Button
+                  component={Link}
+                  to={`/insights/${insight?.id}/edit`}
+                  className="edit"
+                  variant="contained"
+                  color="primary"
+                >
+                  Edit
+                </Button>
+                <Button
+                  className="delete"
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => handleDeleteOpen(insight.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </>
+          )}
+          <hr />
+          <div className="insight-body">
+            <p>{insight?.body}</p>
+          </div>
+          <br />
+          <hr />
+          <div className="buttons">
+            <Button variant="contained" color="secondary" onClick={goBack}>
+              Go Back
+            </Button>
+          </div>
         </div>
-        {insight?.user_id === currentUser?.id && (
-          <>
-            <div className="buttons2">
-              <Button
-                component={Link}
-                to={`/insights/${insight?.id}/edit`}
-                className="edit"
-                variant="contained"
-                color="primary"
-              >
-                Edit
-              </Button>
-              <Button
-                className="delete"
-                variant="contained"
-                color="secondary"
-                onClick={() => handleDelete(insight.id)}
-              >
-                Delete
-              </Button>
-            </div>
-          </>
-        )}
-        <hr />
-        <div className="insight-body">
-          <p>{insight?.body}</p>
-        </div>
-        <br />
-        <hr />
-        <div className="buttons">
-          <Button variant="contained" color="secondary" onClick={goBack}>
-            Go Back
-          </Button>
-        </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+      <DeleteInsightFromDetail
+        insight={insight}
+        openDelete={openDelete === insight.id}
+        onDelete={onDelete}
+        handleOpen={handleDeleteOpen}
+        handleDelete={handleDelete}
+        handleClose={handleDeleteClose}
+      />
+    </>
   );
 }
