4a301ecdb3f168fe0a84406163efa85f47d4ada1
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-07 19:19:24 -0500

    added def update in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,18 @@ class UsersController < ApplicationController
     end
   end
 
+
+  def update
+    if @user.update(user_params)
+      render json: {
+        user: @user.attributes.except("password_digest"),
+        token: @token
+        }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
     # Only allow a trusted parameter "white list" through.
