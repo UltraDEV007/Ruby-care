4b204c7eb8a04005b90fe71c16dda30d020e4d94
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-09 00:55:38 -0500

    upd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_085300) do
+ActiveRecord::Schema.define(version: 2020_12_09_055358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_085300) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "gender"
-    t.datetime "birthday"
+    t.date "birthday"
   end
 
   add_foreign_key "affirmations", "users"
