e05d8a41d58854919bebfbbfe3c36cc7646480e8
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-10 07:49:58 -0500

    separated styles for Search in styledsearch.js

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
@@ -7,53 +6,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import TextField from "@material-ui/core/TextField";
 import ClearIcon from "@material-ui/icons/Clear";
 import { DarkModeContext } from "../Context/DarkModeContext";
-import { blue, yellow } from "@material-ui/core/colors";
-
-const Form = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  &::placeholder {
-    color: ${({ darkMode }) => (darkMode === "dark" ? "#fff" : `inherit`)};
-  }
-  .vl {
-    border-left: ${({ darkMode }) =>
-      darkMode === "dark"
-        ? `1px solid ${yellow[700]}`
-        : `1px solid ${blue[500]}`};
-    height: 40px;
-    margin-right: 10px;
-  }
-
-  .icon {
-    position: absolute;
-    right: 10px;
-  }
-  input {
-    width: 68vw;
-    font-size: 18px;
-    letter-spacing: 0.1px;
-    padding: 12px;
-    border: ${({ darkMode }) =>
-      darkMode === "dark"
-        ? `1px solid ${yellow[700]}`
-        : `1px solid ${blue[500]}`};
-    text-align: left;
-    box-shadow: 5px 5px peachpuff;
-    box-shadow: ${({ darkMode }) =>
-      darkMode === "dark" ? `5px 5px${yellow[700]}` : `5px 5px ${blue[500]}`};
-  }
-  @media screen and (min-width: 1200px) {
-    input {
-      width: 50vw;
-    }
-  }
-  input:focus {
-    outline: none;
-  }
-`;
+import Form from "./styledSearch";
 
 function Search({ search, setSearch }) {
   const [darkMode] = useContext(DarkModeContext);
---
+++ client/src/components/Search/styledSearch.js
@@ -0,0 +1,49 @@
+import { blue, yellow } from "@material-ui/core/colors";
+import styled from "styled-components";
+
+const Form = styled.Div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &::placeholder {
+    color: ${({ darkMode }) => (darkMode === "dark" ? "#fff" : `inherit`)};
+  }
+  .vl {
+    border-left: ${({ darkMode }) =>
+      darkMode === "dark"
+        ? `1px solid ${yellow[700]}`
+        : `1px solid ${blue[500]}`};
+    height: 40px;
+    margin-right: 10px;
+  }
+
+  .icon {
+    position: absolute;
+    right: 10px;
+  }
+  input {
+    width: 68vw;
+    font-size: 18px;
+    letter-spacing: 0.1px;
+    padding: 12px;
+    border: ${({ darkMode }) =>
+      darkMode === "dark"
+        ? `1px solid ${yellow[700]}`
+        : `1px solid ${blue[500]}`};
+    text-align: left;
+    box-shadow: 5px 5px peachpuff;
+    box-shadow: ${({ darkMode }) =>
+      darkMode === "dark" ? `5px 5px${yellow[700]}` : `5px 5px ${blue[500]}`};
+  }
+  @media screen and (min-width: 1200px) {
+    input {
+      width: 50vw;
+    }
+  }
+  input:focus {
+    outline: none;
+  }
+`;
+export default Form;
