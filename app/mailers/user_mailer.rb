566f91e47b6e6123fb5f6d2dc06afda7783f5912
UltraDEV007 <LeTuan2024@outlook.com>
2020-12-14 22:53:37 -0500

    updated sign up mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
  def sign_up_email 
   @user = params[:user]
-  mail(to: @user.email, subject: 'Welcome to Care!')
+  mail(to: @user.email, subject: "Welcome to Care #{@user.name}!")
  end
  def update_account_email
   @user = params[:user]
