a3a6f7370c84cd42fa597580ad4beb9c6f178b15
UltraDEV007 <LeTuan2024@outlook.com>
2021-03-04 14:51:08 -0500

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { Paper } from "@material-ui/core";
 import { ThemeStateProvider } from "./context/ThemeStateContext";
 import FirefoxBrowser from "./screens/Error/FirefoxBrowser";
-import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import { verifyUser } from "./services/auth";
 import { useStateValue } from "./context/CurrentUserContext";
@@ -11,14 +10,10 @@ import AppRouter from "./Router/AppRouter";
 
 function App() {
   const [, dispatch] = useStateValue();
-  // const history = useHistory();
   useEffect(() => {
     const handleVerify = async () => {
       const userData = await verifyUser();
       dispatch({ type: "SET_USER", currentUser: userData });
-      // if (!userData) {
-      //   history.push("/login");
-      // }
     };
     handleVerify();
   }, [dispatch]);
