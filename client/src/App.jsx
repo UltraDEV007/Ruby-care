647f81c779e04f0dda86dd6d7ad67e667292abd2
UltraDEV007 <LeTuan2024@outlook.com>
2021-03-06 10:04:18 -0500

    update APp.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { Paper } from "@material-ui/core";
 import { ThemeStateProvider } from "./context/ThemeStateContext";
 import FirefoxBrowser from "./screens/Error/FirefoxBrowser";
@@ -9,11 +10,23 @@ import AppRouter from "./Router/AppRouter";
 
 function App() {
   const [, dispatch] = useStateValue();
+  const { push } = useHistory();
+  const { pathname } = useLocation();
 
   useMemo(async () => {
     const userData = await verifyUser();
     dispatch({ type: "SET_USER", currentUser: userData });
-  }, [dispatch]);
+
+    if (
+      !userData &&
+      !pathname.match(/^\/login$/i) &&
+      !pathname.match(/^\/register$/i)
+    ) {
+      /* PrivateRoute.jsx should take care of this for lines 22-27, but... 
+      This is for the case when heroku is deciding to hibernate big time. */
+      push("/login");
+    }
+  }, [dispatch, push, pathname]);
 
   if (isUsingFirefox) {
     return <FirefoxBrowser firefoxAgent={isUsingFirefox} />;
