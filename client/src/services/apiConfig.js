568b5710ad0b68fe882f9b3b752b04d25bc23437
UltraDEV007 <LeTuan2024@outlook.com>
2021-08-05 15:00:27 -0400

    update apiConfig.js

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-// const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://heroku-care-api.herokuapp.com/"
-//     : "http://localhost:3000";
-
-const baseUrl = "https://heroku-care-api.herokuapp.com/";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://heroku-care-api.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
